317e5412f5573ac5f67d258ee18ac25a52414555
ralphmaron <ralphmaron.dev@gmail.com>
2024-04-23 18:39:44 +0800

    modified login-signup

--- client/src/presentation/auth/LoginSignUp.jsx
+++ client/src/presentation/auth/LoginSignUp.jsx
@@ -7,6 +7,17 @@ import { useState } from 'react'
 
 function LoginSignUp() {
     const [action, setAction] = useState('Sign Up');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const printValue = () => {
+        console.log(`Name: ${name.value}, Email: ${email.value}, Password: ${password.value}`);
+    }
+
+    // setName = (e) => {
+
+    // }
 
     return (
         <div className='container'>
@@ -20,16 +31,16 @@ function LoginSignUp() {
                     <div></div> :
                     <div className='input'>
                         <img src={user_icon} alt='user_icon' />
-                        <input type='text' placeholder='Name' />
+                        <input type='text' placeholder='Name' onChange={setName} value={name.value} />
                     </div>
                 }
                 <div className='input'>
                     <img src={email_icon} alt='email_icon' />
-                    <input type='email' placeholder='Email ID' />
+                    <input type='email' placeholder='Email ID' onChange={setEmail} value={email.value} />
                 </div>
                 <div className='input'>
                     <img src={password_icon} alt='password_icon' />
-                    <input type='password' placeholder='Password' />
+                    <input type='password' placeholder='Password' onCanPlay={setPassword} value={password.value} />
                 </div>
             </div>
 
@@ -43,10 +54,12 @@ function LoginSignUp() {
                 <div className={action === 'Login' ? 'submit gray' : 'submit'}
                     onClick={() => {
                         setAction('Sign Up')
+                        printValue();
                     }}>Sign Up</div>
                 <div className={action === 'Sign Up' ? 'submit gray' : 'submit'}
                     onClick={() => {
                         setAction('Login')
+                        printValue();
                     }}>Login</div>
             </div>
         </div>
